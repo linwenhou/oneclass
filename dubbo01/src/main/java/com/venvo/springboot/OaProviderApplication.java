843dca2731db9b8033343c4e87e73d2d9995e142
venvo <m13434344340@163.com>
2021-08-12 17:00:30 +0800

    [+]聚合

---
+++ dubbo01/src/main/java/com/venvo/springboot/OaProviderApplication.java
@@ -0,0 +1,13 @@
+package com.venvo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaProviderApplication.class, args);
+	}
+
+}

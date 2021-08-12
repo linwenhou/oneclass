f16f61643d8ea8459888ae9c5b9ec174deadbb27
venvo <m13434344340@163.com>
2021-08-12 17:04:16 +0800

    [+]dubbo-customer/provider

---
+++ dubbo-customer/src/test/java/com/venvo/springboot/dubbo/dubbocustomer/DubboCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venvo.springboot.dubbo.dubbocustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dubbo-proivder/src/main/java/com/venvo/dubbo/DubboProivderApplication.java
@@ -0,0 +1,13 @@
+package com.venvo.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboProivderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProivderApplication.class, args);
+    }
+
+}

0d568739f145c5a37f8ddaeda85ca630d8f8ec21
venvo <m13434344340@163.com>
2021-09-05 19:40:55 +0800

    [+]eureka集群

---
+++ eureka/src/main/java/com/venvo/eureka/csrf/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.venvo.eureka.csrf;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author venvo
+ * @date 2021-09-05 16:56
+ * @description
+ * @modified By
+ * @version: jdk1.8
+ */
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        super.configure(http);
+//    }
+//
+//}
